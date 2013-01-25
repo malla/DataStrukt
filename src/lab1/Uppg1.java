740dc47b8bc56c1837c68562f1a77a6ee2717d2c
Madeleine Appert <m.appert@hotmail.com>
2013-01-25 15:37:14 +0100

    Started

--- src/lab1/Uppg1.java
+++ src/lab1/Uppg1.java
@@ -1,5 +1,41 @@
 package lab1;
 
+import java.util.ArrayList;
+
 public class Uppg1 {
-//WOOOOP! It works! :)
+
+	ArrayList<String> listStrings;
+	
+	public void  Uppg(){
+		listStrings=new ArrayList<String>(10);
+	}
+	
+	public void  Uppg(int i){
+		listStrings=new ArrayList<String>(i);
+	}
+	
+	public void addFirst(String element){
+		listStrings.add(0, element);
+	}
+	
+	public boolean empty(){
+		
+		return true;
+	}
+	
+	public String getFirst(){
+		return "";
+	}
+	
+	public void removeFirst(){
+		
+	}
+	
+	public boolean existP(String elem){
+		return true;
+	}
+	
+	public String toString(){
+		return "";
+	}
 }
