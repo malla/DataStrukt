8c12111e5dceeaa00e5610e87546841c9aacb30d
Madeleine Appert <m.appert@hotmail.com>
2013-02-21 15:15:55 +0100

    Working on DirectedGraph

--- src/Lab3/DirectedGraph.java
+++ src/Lab3/DirectedGraph.java
@@ -1,23 +1,34 @@
+/*
+ * Om e.from är 1, är det nod 1 då, eller bör man lägga in på plats( e.from-1 )när man lägger till?
+ * 
+ * Ska man skapa en tvåriktad väg melan noderna?
+ */
 package lab3;
 
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
+	private ArrayList<Integer>[] nodes;
 
 
+	@SuppressWarnings("only integers will be added")
 	public DirectedGraph(int noOfNodes) {
+		nodes=new ArrayList[noOfNodes];
 		;
 	}
 
 	public void addEdge(E e) {
-		;
+		if (nodes[e.from]==null){
+			nodes[e.from]= new ArrayList<Integer>();
+		}
+		nodes[e.from].add(e.to);
 	}
 
-	public Iterator<E> shortestPath(int from, int to) {
+	public Iterator<E> shortestPath(int from, int to) {//Malla
 		return null;
 	}
 		
-	public Iterator<E> minimumSpanningTree() {
+	public Iterator<E> minimumSpanningTree() {//Bella
 		return null;
 	}
 
