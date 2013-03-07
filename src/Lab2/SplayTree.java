5d400588a36009e799f13dfac0aa5c8f16fd8bba
Bella <isabellefrolich@hotmail.com>
2013-03-07 14:06:23 +0100

    Nothing much

--- src/Lab2/SplayTree.java
+++ src/Lab2/SplayTree.java
@@ -1,20 +1,30 @@
+//Grupp 26
+//Madeleine Appert 891110-4845
+//Isabelle Frölich 900831-2846
+
 package lab2;
 
 import testSortCol.CollectionWithGet;
+
 import datastructures.BinarySearchTree;
 
 //*** När en Nod splayas, ska man sätta dess parent till null? Eller låter man dess gamla parent ligga kvar?
 //*** Kan en nod som är mormor (alltså har ett vänsterbarn som har barn) vara utan ett högerbarn?
 //Alltså:
 //*
-public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree<E> implements CollectionWithGet<E>{
+public class SplayTree<E extends Comparable<? super E>> 
+extends BinarySearchTree<E> implements CollectionWithGet<E>{
 	public SplayTree() {
 		super();
-	}
-
+	}	
+	
 	/*
-	 * Zig - Rotera 1 steg i högervarv, dvs x' y' / \ / \ y' C --> A x' / \ / \
-	 * A B B C
+	 * Zig - Rotera 1 steg i högervarv, dvs 
+	 * 		 x'		    y' 
+	 *		/ \ 	   / \ 
+	 *	   y'  C  --> A   x' 
+	 *	  / \            /  \
+	 *	 A 	 B          B    C
 	 */
 	private void zig(Entry x) {
 		Entry y = x.left;
@@ -31,8 +41,12 @@ public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree
 		x.right = y;
 	} // rotateRight
 	/*
-	 * Zag - Rotera 1 steg i vänstervarv, dvs x' y' / \ / \ A y' --> x' C / \ /
-	 * \ B C A B
+	 * Zag - Rotera 1 steg i vänstervarv, dvs 
+	 *    x'         y' 
+	 *   / \        / \ 
+	 *  A  y' -->  x' C 
+	 *    / \     /\
+	 *   B   C   A  B
 	 */
 
 	private void zag(Entry x) {
@@ -49,9 +63,16 @@ public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree
 			y.left.parent = y;
 		x.left = y;
 	} // rotateLeft
+
 	/*
-	 * ZigZag - Rotera 1 steg i högervarv, 1 steg i vänstervarv, dvs x' z' / \ /
-	 * \ A y' --> x' y' / \ / \ / \ z D A B C D / \ B C
+	 * ZigZag - Rotera 1 steg i högervarv, 1 steg i vänstervarv, dvs
+	 *        x' 			z' 
+	 *       / \		   / \ 
+	 *      A   y' -->    x'  y' 
+	 *         / \ 		 / \  / \ 
+	 *   	  z   D 	A  B C   D 
+	 *  	 / \ 
+	 * 		B   C
 	 */
 
 	private void zigzag(Entry x) {
@@ -69,9 +90,16 @@ public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree
 		x.left = z;
 		z.parent = x;
 	} // zigzag
+
 	/*
-	 * ZagZig - Rotera 2 steg i högervarv, dvs x' z' / \ / \ y' D --> y' x' / \
-	 * / \ / \ A z' A B C D / \ B C
+	 * ZagZig - Rotera 2 steg i högervarv, dvs 
+	 * 			x' 		 	 z' 
+	 * 		   / \ 		   /   \ 
+	 * 		  y'  D -->   y'    x' 
+	 * 		 / \		 / \   / \ 
+	 * 		A 	z' 		A   B C   D 	
+	 * 			/ \
+	 *  		B C
 	 */
 
 	private void zagzig(Entry x) {
@@ -89,9 +117,18 @@ public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree
 		x.right = z;
 		z.parent = x;
 	} // zagzig
+
 	/*
 	 * ZigZig - Rotera 2 steg i högervarv, dvs först genom mormor, sedan mamma
-	 * x' z' / \ / \ y' D --> A y' / \ / \ z C B x / \ / \A B C D
+	 * 
+	 *       x' 		   	z'  
+	 *      / \ 		 	/ \ 
+	 *     y' D 	--> 	A y' 
+	 *    / \				 / \ 
+	 * 	 z   C 			    B x 
+	 * 	/ \ 				 / \
+	 * A   B 				C   D
+	 * 
 	 */
 
 	private void zigzig(Entry x) {
@@ -117,7 +154,13 @@ public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree
 
 	/*
 	 * ZagZag - Rotera 2 steg i vänstervarv, dvs först genom mormor, sedan mamma
-	 * x' z' / \ / \ A y' --> y' D' / \ / \ B z x C / \ / \ C D A B
+	 *   x'                z' 
+	 *  / \               / \ 
+	 * A   y'    -->     y' D' 
+	 *    / \ 	        / \ 
+	 *    B  z 	       x   C 
+	 *      / \ 	  / \ 
+	 *     C   D	 A   B
 	 */
 	private void zagzag(Entry x) {
 		Entry y = x.right, z = x.right.right;
@@ -140,72 +183,98 @@ public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree
 		y.left = z;
 	}
 
-	private void splayToRoot(Entry node) { // Beöver man faktiskt tilldela root?
-											// Eller om
+	/**
+	 * Splays the given Entry
+	 * @param e is the Entry which is splayed, ie brought to the top of the tree
+	 * @returns the node that is splayed, or null, if entry e was null.
+	 */
+	private Entry splayToRoot(Entry e) { // Beöver man faktiskt tilldela root?
+		Entry node=e;			// Eller om
 		if (null != node) {
 			while (node != root) {
 				if (isLeftChild(node)) {
 					if (node.parent == root) {
 						zig(node.parent);
+						node=node.parent;
 					}
-					if (isLeftChild(node.parent)) {
+					else if (isLeftChild(node.parent)) { //Hence is a left child of a left child
 						zigzig(node.parent.parent);
-					} else
+						node=node.parent.parent;
+					} else{
 						zigzag(node.parent.parent);
+						node=node.parent;
+					}
 				} else {// hence is a right Child
 					if (node.parent == root) {
 						zag(node.parent);
+						node=node.parent;
 					}
-					if (isRightChild(node.parent)) {
+					else if (isRightChild(node.parent)) {
 						zagzag(node.parent.parent);
-					} else
+						node=node.parent.parent;
+
+					} else{
 						zagzig(node.parent.parent);
+						node=node.parent;
+					}
 				}
 			}
+			root=node;
+			System.out.println(""+root.element);
+			return node;
 		}
+		return null;
 	}
-
+	
+	/*
+	 * Help method for splay
+	 */
 	private boolean isLeftChild(Entry node) {
 		return (node.parent.left == node);
 	}
 
+	/*
+	 * Help method for splay
+	 */
 	private boolean isRightChild(Entry node) {
 		return (node.parent.right == node);
 	}
 
-	public E get(E elem) {
-		if (null == elem) {
+
+
+	/**Using the recursive find() method in the super class, finds the given element. 
+	 * If the Entry or the element given is null, the method will return null.
+	 * If the element isn't found, the last checked node will be splayed.
+	 * If the element is found, the node containing it will be splayed.
+	 * @param elem is the element which is searched for.
+	 * @param entry is the node in which the search will commence.
+	 * @return Returns the node containing the elem or null if not found.
+	 */
+	@Override
+	protected Entry find(E elem, Entry entry) {
+		if (null == entry || null==elem)
 			return null;
-		} else {
-			Entry node = find(elem);
-			if (null == node) {
-				return null;
-			}
-			return node.element;
+		Entry node = super.find(elem, entry);
+		Entry prev=node;
+
+		// Splay parent of external node if search was unsuccessful
+		if (node == null) {
+			splayToRoot(prev);
 		}
+		splayToRoot(entry);
+		return node;
 	}
 
-	private Entry find(E elem) {
-		if (null == root)
+	public E get(E elem) {
+		if (null == elem || null==root) {
 			return null;
-		Entry node = root, prev;
-		int check = node.element.compareTo(elem);
-		while (check != 0) {
-			prev = node;
-			if (check < 0) {
-				prev = node;
-				node = node.left;
-			} else {
-				prev = node;
-				node = node.right;
-			}
+		} else {
+			Entry node = find(elem, root);
 			if (null == node) {
-				splayToRoot(prev);
 				return null;
 			}
-			check = node.element.compareTo(elem);
+			splayToRoot(node);
+			return node.element;
 		}
-		splayToRoot(node);
-		return node;
 	}
 }
--- src/Lab3/DirectedGraph.java
+++ src/Lab3/DirectedGraph.java
@@ -1,98 +1,69 @@
 /*
  * Om e.from är 1, är det nod 1 då, eller bör man lägga in på plats( e.from-1 )när man lägger till?
- * 
+ *
  * Ska man skapa en tvåriktad väg melan noderna?
  */
-
 //Grupp 26
 //Madeleine Appert 891110-4845
 //Isabelle Frölich 900831-2846
-
 package lab3;
 
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
-	private ArrayList<BusEdge>[] nodes;
 	private int numberOfNodes;
 	private List<E>[] edges;
 
 	public DirectedGraph(int noOfNodes) {
-		nodes = new ArrayList[noOfNodes];
 		edges = (List<E>[]) new LinkedList[noOfNodes];
 		numberOfNodes = noOfNodes;
 		for (int i = 0; i < noOfNodes; i++) {
 			edges[i] = new LinkedList<E>();
 		}
-
 	}
 
 	public void addEdge(E e) {
-		// if (nodes[e.from]==null){
-		// nodes[e.from]= new ArrayList<BusEdge>();
-		// }
-		// nodes[e.from].add((BusEdge) e);
 		edges[e.from].add(e);
 	}
 
-	public Iterator<E> shortestPath(int from, int to) {// Malla
-		// ArrayList<Integer> vMinusS= new ArrayList<Integer>(); //Alla noder
-		// som ej
-		// beräknats ifrån.
-		// for (int i=0;i>nodes.length;i++){
-		// vMinusS.add(i, i);
-		// }
-		// ArrayList<Integer> s= new ArrayList<Integer>(); //Alla noder som har
-		// beräknats
-		PriorityQueue<ComparableDjikstraPath> results = new PriorityQueue<ComparableDjikstraPath>();
-		int node = from;
-		double cost = 0;
-		LinkedList<Integer> path = new LinkedList<Integer>();
-		results.add(new ComparableDjikstraPath(node, cost, path));
+	public Iterator<E> shortestPath(int from, int to) { // Malla
+		PriorityQueue<ComparableDijkstraPath> results = new PriorityQueue<ComparableDijkstraPath>();
 		ArrayList<Integer> visitedNodes = new ArrayList<Integer>();
+		int node = from;
+		results.add(new ComparableDijkstraPath(node, 0, new LinkedList<E>()));
+		ComparableDijkstraPath djikstra;
 		while (!results.isEmpty()) {
-			ComparableDjikstraPath djikstra = results.remove();
-			node = djikstra.to;
+			djikstra = results.poll();
+			node = djikstra.node;
 			if (!visitedNodes.contains(node)) {
 				if (node == to) {
-					/**
-					 * return en iterator hur man kom till node.
-					 */
+					return djikstra.path.iterator();
+				} else {
+					visitedNodes.add(node);
+					for (int i = 0; i < edges[node].size(); i++) {
+						E connection = edges[node].get(i);
+						int connectionNode = connection.to;
+						double totalCost = connection.getWeight()
+								+ djikstra.cost;
+						LinkedList<E> newPath = new LinkedList<E>(djikstra.path);
+						newPath.add(connection);
+						results.add(new ComparableDijkstraPath(connectionNode,
+								totalCost, newPath));
+					}
 				}
 			}
-			visitedNodes.add(node);
-			for (int i = 0; i < nodes[node].size(); i++) {
-				int tempToNode = nodes[node].get(i).to;
-				double weight = nodes[node].get(i).getWeight();
-				LinkedList<Integer> tempPath = djikstra.path;
-				tempPath.add(node);
-				results.add(new ComparableDjikstraPath(tempToNode, weight,
-						tempPath));
-			}
 		}
-		// //Vi börjar kolla...
-		// while(fromNode!=to){
-		// vMinusS.set(fromNode, null);
-		// s.set(fromNode, fromNode);
-		// for(int i=0; i<nodes[fromNode].size();i++){
-		// double weight=nodes[fromNode].get(i).getWeight();
-		// int toNode=nodes[fromNode].get(i).to;
-		// d.set(toNode, weight);
-		// v.set(toNode, fromNode);
-		// if (weight)
-		// }
-		// }
 		return null;
 	}
 
-	public class ComparableDjikstraPath implements Comparable {
-		private int to;
+	@SuppressWarnings("rawtypes")
+	public class ComparableDijkstraPath implements Comparable {
+		private int node;
 		private double cost;
-		private LinkedList<Integer> path;
+		private LinkedList<E> path;
 
-		public ComparableDjikstraPath(int to, double cost,
-				LinkedList<Integer> path) {
-			this.to = to;
+		public ComparableDijkstraPath(int to, double cost, LinkedList<E> path) {
+			this.node = to;
 			this.cost = cost;
 			this.path = path;
 		}
@@ -104,7 +75,8 @@ public class DirectedGraph<E extends Edge> {
 			} else if (comp.getClass().isInstance(this.getClass())) {
 				throw new IllegalArgumentException();
 			}
-			ComparableDjikstraPath compareTo = (ComparableDjikstraPath) comp;
+			@SuppressWarnings("unchecked")
+			ComparableDijkstraPath compareTo = (ComparableDijkstraPath) comp;
 			if (this.cost == compareTo.cost) {
 				return 0;
 			} else if (this.cost > compareTo.cost) {
@@ -114,9 +86,15 @@ public class DirectedGraph<E extends Edge> {
 		}
 	}
 
+	/**
+	 * CompKruskalEdge can compare different edges, get their To, From and
+	 * weight.
+	 * 
+	 * @param <E>
+	 */
+
 	public class CompKruskalEdge<E extends Edge> implements
 			Comparable<CompKruskalEdge<E>> {
-
 		private E edge;
 
 		protected CompKruskalEdge(E e) {
@@ -141,9 +119,8 @@ public class DirectedGraph<E extends Edge> {
 		}
 
 		@Override
-		public int compareTo(CompKruskalEdge<E> otherEdge) {
-			return (int) (getWeight() - otherEdge.getWeight());
-
+		public int compareTo(CompKruskalEdge<E> edge) {
+			return (int) (getWeight() - edge.getWeight());
 		}
 	}
 
@@ -153,45 +130,40 @@ public class DirectedGraph<E extends Edge> {
 	 * 
 	 * @Return Returns an iterator over the minimum spanning tree
 	 */
-
 	public Iterator<E> minimumSpanningTree() {// Bella
 		List<E>[] cc = (List<E>[]) new LinkedList[this.numberOfNodes];
 		PriorityQueue<CompKruskalEdge> pq = new PriorityQueue<CompKruskalEdge>();
-
 		// Add a list to each node in the array
 		for (int i = 0; i < numberOfNodes; i++) {
 			cc[i] = new LinkedList<E>();
 		}
-
 		// All edges are added into a priorityqueue
-		for (List<E> edgeList : this.edges) {
-			for (E edge : edgeList) {
+		for (int i = 0; i < edges.length; i++) {
+			for (E edge : edges[i]) {
 				pq.add(new CompKruskalEdge<E>(edge));
 			}
 		}
-
 		int longest;
 		int shortest;
-
 		// As long as there are elements left in the priorityqueue, continue.
 		while (!pq.isEmpty()) {
+			@SuppressWarnings("unchecked")
 			CompKruskalEdge<E> tempEdge = pq.poll();
-
 			// Checks so that it doesn't point to the same element
 			if (cc[tempEdge.getTo()] != cc[tempEdge.getFrom()]) {
 				// Checks so that the list doesn't already contain the element
 				if (!(cc[tempEdge.getFrom()].contains(tempEdge) && cc[tempEdge
 						.getTo()].contains(tempEdge))) {
 					// Sets the shortest and the longest edge
-					if (cc[tempEdge.getFrom()].size() >= cc[tempEdge.getTo()]
+					if (cc[tempEdge.getFrom()].size() <= cc[tempEdge.getTo()]
 							.size()) {
-						longest = tempEdge.getFrom();
-						shortest = tempEdge.getTo();
-					} else {
 						longest = tempEdge.getTo();
 						shortest = tempEdge.getFrom();
+						
+					} else {
+						longest = tempEdge.getFrom();
+						shortest = tempEdge.getTo();
 					}
-
 					// Transfer the elements and refer the nodes in cc to the
 					// refreshed list.
 					for (E edge : cc[shortest]) {
@@ -199,11 +171,8 @@ public class DirectedGraph<E extends Edge> {
 						cc[edge.to] = cc[longest];
 						cc[edge.from] = cc[longest];
 					}
-
 					cc[longest].add(tempEdge.getEdge());
-
 					cc[shortest] = cc[longest];
-
 					// Return the iterator if the number of elements it is
 					// containing is
 					// the number of nodes minus one.
@@ -213,7 +182,8 @@ public class DirectedGraph<E extends Edge> {
 				}
 			}
 		}
+		// Otherwise return null
 		return null;
 	}
-
 }
+
